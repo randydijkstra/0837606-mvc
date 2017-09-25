a1e9a532198a11022dab656fc1681995794a4817
RandyDev <randydijkstra92@gmail.com>
2017-09-25 14:53:18 +0200

    Update user seeder with roles

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,37 +13,42 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+      $role_player = Role::where('name', 'player')->first();
+      $role_admin  = Role::where('name', 'admin')->first();
+
       //Create admin
-      DB::table('users')->insert([
-        'firstname' => 'Randy',
-        'lastname' => 'Dijkstra',
-        'email' => 'randydijkstra92@gmail.com',
-        'password' => 'admin1234',
-        'role_id' => 1
-      ]);
+      $admin = new User();
+      $admin->firstname = 'Randy';
+      $admin->lastname = 'Dijkstra';
+      $admin->email = 'randydijkstra92@gmail.com';
+      $admin->password = bcrypt('admin1234');
+      $admin->save();
+      $admin->roles()->attach($role_admin);
 
       //Create users
-      DB::table('users')->insert([
-        'firstname' => 'Jan',
-        'lastname' => 'Jansen',
-        'email' => 'email'.str_random(5).'@email.com',
-        'password' => 'test1234'
-        'role_id' => 0
-      ]);
-      DB::table('users')->insert([
-        'firstname' => 'Hendrik',
-        'lastname' => 'Hendriksen',
-        'email' => 'email'.str_random(5).'@email.com',
-        'password' => 'test1234',
-        'role_id' => 0
+      $player = new User();
+      $player->firstname = 'Jan';
+      $player->lastname = 'Jansen';
+      $player->email = 'email'.str_random(5).'@email.com';
+      $player->password = bcrypt('pass');
+      $player->save();
+      $player->roles()->attach($role_player);
 
-      ]);
-      DB::table('users')->insert([
-        'firstname' => 'Piet',
-        'lastname' => 'Pietersen',
-        'email' => 'email'.str_random(5).'@email.com',
-        'password' => 'test1234',
-        'role_id' => 0
-      ]);
+      $player = new User();
+      $player->firstname = 'Hendrik';
+      $player->lastname = 'Hendriksen';
+      $player->email = 'email'.str_random(5).'@email.com';
+      $player->password = bcrypt('pass');
+      $player->save();
+      $player->roles()->attach($role_player);
+
+      $player = new User();
+      $player->firstname = 'Pieter';
+      $player->lastname = 'Pietersen';
+      $player->email = 'email'.str_random(5).'@email.com';
+      $player->password = bcrypt('pass');
+      $player->save();
+      $player->roles()->attach($role_player);
+
     }
 }
